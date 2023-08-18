938e6a40fa1ba2c8582b1ecf4904886c7de76ae3
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2023-08-18 11:28:00 +0530

    chore: setup swagger

--- the_chimera_intergalactic-backend/src/main.ts
+++ the_chimera_intergalactic-backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle('The Chimera Intergalactic API')
+    .setDescription('The Chimera Intergalactic API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
