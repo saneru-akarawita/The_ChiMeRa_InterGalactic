bd9a1be61bfca7871d66450a5513dd3b00eca012
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-08-19 19:24:53 +0530

    fixed linter warnings and errors

--- the_chimera_intergalactic-backend/src/auth/dto/login.dto.ts
+++ the_chimera_intergalactic-backend/src/auth/dto/login.dto.ts
@@ -1,35 +1,40 @@
-import { IsBoolean, IsBooleanString, IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
-import { DoesAccoutnExist } from 'src/validators'
+import {
+  IsBoolean,
+  IsEmail,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { DoesAccoutnExist } from 'src/validators';
 
 export class TravelerLoginDto {
+  @DoesAccoutnExist('TRAVELER', {
+    message: 'Email is not associated with an intergalactic traveler account',
+  })
+  @IsEmail()
+  readonly email: string;
 
-    @DoesAccoutnExist('TRAVELER', { message: "Email is not associated with an intergalactic traveler account" })
-    @IsEmail()
-    readonly email: string;
+  @MaxLength(12, { message: `Password doesn't match` })
+  @MinLength(8, { message: `Password doesn't match` })
+  @IsString()
+  readonly password: string;
 
-    @MaxLength(12, { message: `Password doesn't match` })
-    @MinLength(8, { message: `Password doesn't match` })
-    @IsString()
-    readonly password: string;
-
-
-    @IsBoolean()
-    readonly remember_me: boolean;
+  @IsBoolean()
+  readonly remember_me: boolean;
 }
-
 
 export class AdminLoginDto {
+  @DoesAccoutnExist('ADMIN', {
+    message: 'Email is not associated with an intergalactic admin account',
+  })
+  @IsEmail()
+  readonly email: string;
 
-    @DoesAccoutnExist('ADMIN', { message: "Email is not associated with an intergalactic admin account" })
-    @IsEmail()
-    readonly email: string;
+  @MaxLength(12, { message: `Password doesn't match` })
+  @MinLength(8, { message: `Password doesn't match` })
+  @IsString()
+  readonly password: string;
 
-    @MaxLength(12, { message: `Password doesn't match` })
-    @MinLength(8, { message: `Password doesn't match` })
-    @IsString()
-    readonly password: string;
-
-    @IsBoolean()
-    readonly remember_me: boolean;
+  @IsBoolean()
+  readonly remember_me: boolean;
 }
-
--- the_chimera_intergalactic-backend/src/validators/unique-email.validator.ts
+++ the_chimera_intergalactic-backend/src/validators/unique-email.validator.ts
@@ -1,12 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-import {
-    registerDecorator,
-    ValidationOptions,
-    ValidatorConstraint,
-    ValidatorConstraintInterface,
-} from 'class-validator';
 import { PrismaClient } from '@prisma/client';
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
 
 /**
  * To check if the email is already existing in a user account
@@ -14,40 +14,41 @@ import { PrismaClient } from '@prisma/client';
  * @constructor
  */
 export function IsEmailUnique(validationOptions?: ValidationOptions) {
-    return function (object: any, propertyName: string) {
-        registerDecorator({
-            target: object.constructor,
-            propertyName: propertyName,
-            options: validationOptions,
-            validator: NotExistingEmailValidation,
-        });
-    };
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: NotExistingEmailValidation,
+    });
+  };
 }
 
 @ValidatorConstraint({ name: 'email', async: true })
 @Injectable()
 export class NotExistingEmailValidation
-    implements ValidatorConstraintInterface {
-    prisma: PrismaClient;
+  implements ValidatorConstraintInterface
+{
+  prisma: PrismaClient;
 
-    constructor() {
-        this.prisma = new PrismaClient();
-    }
-    async validate(value: string): Promise<boolean> {
-        return this.prisma.user
-            .findFirst({ where: { email: value } })
-            .then((user) => {
-                if (user) {
-                    // throw new BadRequestException({
-                    //     message: ['Email already exists'],
-                    //     error: 'Bad Request',
-                    //     statusCode: 400,
-                    // });
-                    // instead of throwing right away, add to the class-validator error list so that it can be returned as a whole with the rest of the errors
-                    return false;
-                } else {
-                    return true;
-                }
-            });
-    }
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  async validate(value: string): Promise<boolean> {
+    return this.prisma.user
+      .findFirst({ where: { email: value } })
+      .then((user) => {
+        if (user) {
+          // throw new BadRequestException({
+          //     message: ['Email already exists'],
+          //     error: 'Bad Request',
+          //     statusCode: 400,
+          // });
+          // instead of throwing right away, add to the class-validator error list so that it can be returned as a whole with the rest of the errors
+          return false;
+        } else {
+          return true;
+        }
+      });
+  }
 }
