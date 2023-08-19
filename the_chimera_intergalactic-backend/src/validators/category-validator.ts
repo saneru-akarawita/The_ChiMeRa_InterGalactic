15358135f7ae8ba69f2564c61668b506c85e7b20
Neethamadhu Madurasinghe <neethamadhu.ma@gmail.com>
2023-08-19 16:50:52 +0530

    Chore: Create category validator

---
+++ the_chimera_intergalactic-backend/src/validators/category-validator.ts
@@ -0,0 +1,30 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'isValidType', async: false })
+export class IsValidTypeConstraint implements ValidatorConstraintInterface {
+  validate(type: string, args: ValidationArguments) {
+    return args.constraints[0].includes(type) ? true : false;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Invalid type';
+  }
+}
+
+export function IsValidType(property: Array<string>, validationOptions?: ValidationOptions) {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: IsValidTypeConstraint,
+    });
+  };
+}
